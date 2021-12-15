f87397671d032451aad3d80889305d185466b4c3
Peter Balivet <peterbalivet@gmail.com>
2021-12-15 20:24:27 +0100

    added pub and priv keys functions

--- client/src/crypto.rs
+++ client/src/crypto.rs
@@ -1,22 +1,45 @@
-use std::fmt::Error;
-use aes::{Aes256Ctr, cipher::{
-    StreamCipher,NewCipher,
-}};
+use aes::{
+    cipher::{NewCipher, StreamCipher},
+    Aes256Ctr,
+};
+use rand::rngs::ThreadRng;
+use rsa::{errors::Error as RsaError, PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+use std::fmt::Error as FmtError;
 
-pub fn aes_256_ctr_encrypt(ptext: &[u8], key: &[u8]) -> Result<Vec<u8>, Error> {
+const RSA_KEY_SIZE: usize = 4096;
+
+pub fn aes_256_ctr_encrypt(ctext: &[u8], key: &[u8]) -> Result<Vec<u8>, FmtError> {
     if key.len() != 32 {
-        return Err(Error);
+        return Err(FmtError);
     }
 
     let zero_nonce = [0u8; 16];
     let mut cipher = Aes256Ctr::new(key.into(), (&zero_nonce).into());
 
-    let mut ctext = ptext.to_vec();
-    cipher.apply_keystream(&mut ctext);
+    let mut vctext = ctext.to_vec();
+    cipher.apply_keystream(&mut vctext);
 
-    Ok(ctext)
+    Ok(vctext)
 }
 
-pub fn aes_256_ctr_decrypt(ctext: &[u8], key: &[u8]) -> Result<Vec<u8>, Error> {
-    aes_256_ctr_encrypt(ctext, key)
+pub fn aes_256_ctr_decrypt(ptext: &[u8], key: &[u8]) -> Result<Vec<u8>, FmtError> {
+    aes_256_ctr_encrypt(ptext, key)
+}
+
+pub fn rsa_gen_keys() -> (RsaPrivateKey, RsaPublicKey) {
+    let mut rng = ThreadRng::default();
+    let private_key = RsaPrivateKey::new(&mut rng, RSA_KEY_SIZE).expect("failed to generate a key");
+    let public_key = RsaPublicKey::from(&private_key);
+    (private_key, public_key)
+}
+
+pub fn rsa_encrypt(ctext: &[u8], pub_key: RsaPublicKey) -> Result<Vec<u8>, RsaError> {
+    let mut rng = ThreadRng::default();
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    pub_key.encrypt(&mut rng, padding, &ctext[..])
+}
+
+pub fn rsa_decrypt(ptext: &[u8], priv_key: RsaPrivateKey) -> Result<Vec<u8>, RsaError> {
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    priv_key.decrypt(padding, &ptext)
 }
